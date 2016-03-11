53161799fb72bf9a0eae9e6a8203d5563991a0cb
GiorgioChiurato <giorgiochiurato@gmail.com>
2016-03-11 19:18:21 +0100

    codice del laser di puntamento v2
    versione 2, input dati da sensore non abilitato

--- progetto commissione/sketch_commissione/sketch_commissione.ino
+++ progetto commissione/sketch_commissione/sketch_commissione.ino
@@ -1,49 +1,48 @@
-#include <SoftwareServo.h> 
-SoftwareServo servo1;
+#include <Servo.h> 
+Servo servo1;
 #include <math.h>
 #define echoPin 7 
 #define trigPin 8 
 
 
 long tempo; 
-int altezza = 1; //cambiare con il valore della distanza tra i laser 
+int altezza = 10; //cambiare con il valore della distanza tra i laser 
 int distanza;
-int angolo1;
+double angolo1;
 int angoloServo;
 
 
 void setup() {
- pinMode(trigPin, OUTPUT);
- pinMode(echoPin, INPUT);
+ //pinMode(trigPin, OUTPUT); inizializzazione pin per sensore
+// pinMode(echoPin, INPUT);
  servo1.attach(4);
  Serial.begin (9600);
 
 }
 
 void loop() {
-  
- digitalWrite(trigPin, LOW); 
+ /*digitalWrite(trigPin, LOW);       istruzioni per la rilevazione dati dal sensore ad ultrasuoni
  delayMicroseconds(2); 
  
  digitalWrite(trigPin, HIGH);
  delayMicroseconds(10); 
  
  digitalWrite(trigPin, LOW);
- tempo = pulseIn(echoPin, HIGH);
- 
- distanza = tempo*0.034/2;
+ tempo = pulseIn(echoPin, HIGH);*/
+
+ distanza = Serial.parseInt();   //tempo*0.034/2; calcolo della distanza in base ai dati del sensore
 
  Serial.print("Distanza: ");
  Serial.println(distanza);
- 
- angolo1 =  atan2(distanza, altezza); 
- if(angolo1 != 0)
- { angoloServo = 90-angolo1;
+ double ratio = distanza/altezza;
+ angolo1 = round (atan(ratio)*180/3.14); 
+ if(angolo1 != 0){
+  angoloServo = 90-angolo1;
  
    servo1.write(angoloServo);
    Serial.print("Angolo: ");
    Serial.println(angoloServo);
-   delay(15); }else
+   delay(2000); } else
    { servo1.write(0);
    Serial.print("Angolo: ");
    Serial.println(0);
